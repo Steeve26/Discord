2de7102e916657aa8397ef6586cf62bf71bcecd2
Estifanos Wendimu <teffsuce@gmail.com>
2024-03-31 01:03:29 +0300

    bug fix test commit

--- src/components/discordServer.jsx
+++ src/components/discordServer.jsx
@@ -15,8 +15,8 @@ export default function discordServer({name, icon, index, type, iconType, backgr
   }
   return (
     <button className={`flex justify-center px-3 active:translate-y-[2px]`} onClick={handleServerSelection}>
-      <div className={`${iconType !== 'image' && 'px-2 py-2'} ${selectedServer === name ? index == 0 && !paths.includes(location.pathname) ? 'rounded-3xl' : 'rounded-2xl bg-' + hoverColor : 'rounded-3xl'} 
-      hover:rounded-2xl ${hoverColor && 'hover:text-white hover:bg-' + hoverColor}
+      <div style={{}} className={`${iconType !== 'image' && 'px-2 py-2'} ${selectedServer === name ? index == 0 && !paths.includes(location.pathname) ? 'rounded-3xl' : 'rounded-2xl !bg-discordBlue' : 'rounded-3xl'} 
+      hover:rounded-2xl ${hoverColor && 'hover:text-white ' + hoverColor}
        size-[47px] overflow-hidden  flex  items-center duration-200 justify-center 
       ${(name == 'default' || type == 'action') && 'bg-highlightLightGrey text-brightGreen'}`}>
       {iconType == 'image' ? <img src={icon} className={`w-full h-full ${background ? background : 'bg-primary'}  duration-200`}/> : icon}
